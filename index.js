425fbe1697f24dd8e12082420c6476143e071ee2
dot-cera <ritaclaris980@gmail.com>
2024-01-10 12:11:29 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let messages =[];
+function writeCards(stringNames, eventName) {
+    for (let i = 0; i < stringNames.length; i++) {
+ let customMessage = `Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`;
+ messages.push(customMessage);
+}
+return messages;
+  }
+
+
+function countDown(number){
+    console.log(number);
+    while (number > 0) {
+        number--;
+        console.log(number);
+      }
+}
